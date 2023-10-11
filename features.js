e2d498e2256d09a52b14ba34aba0cb183bb2bbe4
Kshitij Chandra <52675922+MAX-glitch717@users.noreply.github.com>
2023-10-11 21:42:00 +0100

    Update features.js
    adding a sticky navigation bar and fade in animation

--- features.js
+++ features.js
@@ -6,3 +6,14 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+const navbar = document.getElementById('navbar');
+const sticky = navbar.offsetTop;
+
+window.onscroll = function() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add('sticky');
+    } else {
+        navbar.classList.remove('sticky');
+    }
+};
